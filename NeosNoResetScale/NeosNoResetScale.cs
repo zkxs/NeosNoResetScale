5cf86cafe667b052dc89b1b6ffe79f869b822c8a
Michael Ripley <zkxs00@gmail.com>
2022-01-01 06:40:34 -0600

    Initial commit

---
+++ NeosNoResetScale/NeosNoResetScale.cs
@@ -0,0 +1,126 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace NeosNoResetScale
+{
+    public class NeosNoResetScale : NeosMod
+    {
+        public override string Name => "NeosNoResetScale";
+        public override string Author => "runtime";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/zkxs/NeosNoResetScale";
+
+        private static MethodInfo _startTask;
+        private static MethodInfo _isAtScale;
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.michaelripley.NeosNoResetScale");
+
+            _startTask = AccessTools.DeclaredMethod(typeof(Worker), nameof(Worker.StartTask), new Type[] { typeof(Func<Task>) });
+            if (_startTask == null)
+            {
+                Error("Could not find method Worker.StartTask(Func<Task>)");
+                return;
+            }
+
+            _isAtScale = AccessTools.DeclaredMethod(typeof(UserRoot), nameof(UserRoot.IsAtScale), new Type[] { typeof(float) });
+            if (_isAtScale == null)
+            {
+                Error("Could not find method UserRoot.IsAtScale(float)");
+                return;
+            }
+
+            MethodInfo openContextMenu = AccessTools.DeclaredMethod(typeof(CommonTool), "OpenContextMenu");
+            if (openContextMenu == null)
+            {
+                Error("Could not find method CommonTool.OpenContextMenu(*)");
+                return;
+            }
+
+            MethodInfo asyncMethodConstructor = FindAsyncMethod(PatchProcessor.GetOriginalInstructions(openContextMenu));
+            if (asyncMethodConstructor == null)
+            {
+                Error("Could not find target async block constructor in CommonTool.OpenContextMenu(*)");
+                return;
+            }
+            Debug($"Found async method constructor: \"{asyncMethodConstructor.FullDescription()}\"");
+
+            AsyncStateMachineAttribute asyncAttribute = (AsyncStateMachineAttribute)asyncMethodConstructor.GetCustomAttribute(typeof(AsyncStateMachineAttribute));
+            if (asyncAttribute == null)
+            {
+                Error($"Could not find AsyncStateMachine for \"{asyncMethodConstructor.FullDescription()}\"");
+                return;
+            }
+
+            MethodInfo asyncMethod = AccessTools.DeclaredMethod(asyncAttribute.StateMachineType, "MoveNext", new Type[] { });
+            if (asyncMethod == null)
+            {
+                Error("Could not find target async block in CommonTool.OpenContextMenu(*)");
+                return;
+            }
+            // should be FrooxEngine.CommonTool.'<>c__DisplayClass333_0'.'<<OpenContextMenu>b__0>d'.MoveNext()
+            Debug($"Found async method: \"{asyncMethod.FullDescription()}\"");
+
+            harmony.Patch(asyncMethod, transpiler: new HarmonyMethod(AccessTools.DeclaredMethod(typeof(NeosNoResetScale), nameof(Transpiler))));
+        }
+
+        private static MethodInfo FindAsyncMethod(List<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
+            for (int callIdx = 0; callIdx < codes.Count; callIdx++)
+            {
+                if (codes[callIdx].Calls(_startTask))
+                {
+                    for (int ldftnIdx = callIdx - 1; ldftnIdx >= Math.Max(0, callIdx - 5); ldftnIdx--)
+                    {
+                        if (codes[ldftnIdx].opcode.Equals(OpCodes.Ldftn))
+                        {
+                            return (MethodInfo)codes[ldftnIdx].operand;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            Debug($"There are {instructions.Count()} instructions");
+            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
+            for (int idx = 1; idx < codes.Count; idx++)
+            {
+                CodeInstruction call = codes[idx - 1];
+                CodeInstruction branch = codes[idx];
+
+                if (call.Calls(_isAtScale) && branch.opcode.Equals(OpCodes.Brfalse_S))
+                {
+                    // replace the brfalse with an (unconditional) br
+                    codes[idx] = new CodeInstruction(OpCodes.Br_S, branch.operand);
+
+                    // insert a pop right before our new BR
+                    codes.Insert(idx, new CodeInstruction(OpCodes.Pop));
+
+                    Msg("Transpiler succeeded");
+                    return codes.AsEnumerable();
+                }
+            }
+
+            throw new TranspilerException("Failed to find IL matching if(FrooxEngine.UserRoot::IsAtScale)");
+        }
+
+        private class TranspilerException : Exception
+        {
+            public TranspilerException(string message) : base(message) { }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# NeosNoResetScale
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that removes the Reset Scale functionality from the built in context menu button. This allows you to lock your scale whenever you want. Restoring the reset scale functionality is left as an exercise for the user. If you wish to do it via creating a context menu button, here are the details to replicate the vanilla behavior:
+| Field | Value |
+| - | - |
+| Label | `Interaction.ResetScale` (locale string) or `Reset Scale` (English) |
+| Icon | `neosdb:///38c8ebd9590dbefd2589189cf7e25ea20e25a78d7c675ff613d566bdd5f5afc9.png` |
+| Color (RGB) | (1.0, 0.4, 0.2) |
+| Transition Time | 0.25 seconds |
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+2. Place [NeosNoResetScale.dll](https://github.com/zkxs/NeosNoResetScale/releases/latest/download/NeosNoResetScale.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
